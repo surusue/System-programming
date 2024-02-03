65502d6e2711fe64850c5d180b77bfa4add41c81
surusue <ysh@knu.ac.kr>
2024-02-03 21:04:50 +0900

    Introduction to Shell

---
+++ #8week/sp08A_2021115218.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#echo로 출력하기
+echo "\$0 is script name"
+echo "\$# is number of parameters"
+echo "\$HOME is user's home directory"
+echo "\$PATH is path to find the executable"
+echo "\$PS1 is default prompt variables"
